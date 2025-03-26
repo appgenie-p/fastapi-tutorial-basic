9890e9acecf2308ea09d101ddd6406bf5fcba741
Gennady <gpolejaev@gmail.com>
2025-03-26 11:52:28 +0800

    Refactor FastAPI application: add parameterized endpoints, remove main.py, and update testing configuration

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,25 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+class Param(str, Enum):
+    A = "a"
+    B = "b"
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/{param}")
+async def root_with_param(param):
+    return {"message": f"Hello {param}"}
+
+
+@app.get("/param/{enum_param}")
+async def param_basic_test(enum_param: Param):
+    if enum_param == Param.A:
+        return {"message": f"Hello {enum_param.name}"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,22 @@
 from fastapi.testclient import TestClient
-from src.app.main import app
+from app.main import Param, app
 
 client = TestClient(app)
 
 
-def test_root():
+def test_valid_no_param():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_valid_simple_param():
+    response = client.get("/ABC")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello ABC"}
+
+
+def test_valid_param_A():
+    response = client.get("/param/a")
+    assert response.status_code == 200, response.json()
+    assert response.json() == {"message": f"Hello {Param.A.name}"}
